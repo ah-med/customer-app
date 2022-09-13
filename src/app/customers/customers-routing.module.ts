1cbbb23a72ca714a4c56e60822e01a19264962d9
ahmedomosanya <opeyemiomosanya@gmail.com>
2022-09-13 17:01:37 +0100

    lazy loading

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomersComponent } from './customers.component';
-import { OrdersComponent } from './orders/orders.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
@@ -9,13 +8,20 @@ const routes: Routes = [
     path: '',
     component: CustomersComponent,
     children: [
+      { path: 'overview', loadChildren: () => import('./overview/overview.module').then(m => m.OverviewModule) },
       {
         path: 'orders',
-        component: OrdersComponent,
+        loadChildren: () =>
+          import('./orders/orders.module').then((m) => m.OrdersModule),
       },
       {
-        path: 'profile',
+        path: '',
         component: ProfileComponent,
+      },
+      {
+        path: 'accounts',
+        loadChildren: () =>
+          import('./accounts/accounts.module').then((m) => m.AccountsModule),
       },
     ],
   },
